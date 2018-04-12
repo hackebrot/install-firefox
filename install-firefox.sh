b175e6314b74a7c8c88dd0c2cd917e2b97fc5bfa
Raphael Pierzina <raphael@hackebrot.de>
2018-04-12 14:10:11 +0200

    Start CLI for installing Firefox and Geckodriver

---
+++ install-firefox.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env sh
+
+set -e
+set -u
+
+FIREFOX_VERSION="59.0"
+GECKODRIVER_VERSION="0.20.0"
+
+install_firefox() {
+    echo "Installing Firefox $FIREFOX_VERSION"
+}
+
+
+install_geckodriver() {
+    echo "Installing Geckodriver $GECKODRIVER_VERSION"
+}
+
+main() {
+    while test $# -gt 0; do
+        case "$1" in
+        -f|--firefox)
+            shift
+            if [ $# -gt 0 ]; then
+                export FIREFOX_VERSION="$1"
+                shift
+                continue
+            fi
+            echo "Require value for FIREFOX_VERSION" >&2
+            exit 1
+            ;;
+        -g|--geckodriver)
+            shift
+            if [ $# -gt 0 ]; then
+                export GECKODRIVER_VERSION="$1"
+                shift
+                continue
+            fi
+            echo "Require value for GECKODRIVER_VERSION" >&2
+            exit 1
+            ;;
+        --help|-h)
+            usage
+            exit 0
+            ;;
+        *)
+            ;;
+        esac
+    done
+
+    install_firefox
+    install_geckodriver
+}
+
+usage() {
+    cat 1>&2 <<EOF
+install-firefox 0.1.0
+Installer for Firefox and Geckodriver
+
+USAGE:
+    install-firefox [FLAGS] [OPTIONS]
+
+FLAGS:
+    -h, --help    Show help message
+
+OPTIONS:
+        --firefox [VERSION]        Firefox version to install
+        --geckodriver [VERSION]    Geckodriver version to install
+EOF
+}
+
+
+main "$@" || exit 1
